40be85b68daa09f0cae0ac6188469fb3f7387b85
Rushil Punukollu <rushilpunukolluyt@gmail.com>
2025-10-18 19:15:41 -0600

    Setup environment and add user input for ticker and date

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-import os
+from dotenv import load_dotenv
+import sys  # noqa: E402
+import os  # noqa: E402
+load_dotenv()
 sys.path.append(os.path.join(os.path.dirname(__file__), 'TradingAgents'))
 
 from tradingagents.graph.trading_graph import TradingAgentsGraph
@@ -22,9 +24,18 @@ config["data_vendors"] = {
 # Initialize with custom config
 ta = TradingAgentsGraph(debug=True, config=config)
 
-
-
-
 # forward propagate
-_, decision = ta.propagate("NVDA", "2024-05-10")
-print(decision)
+print("What ticker do you want to trade?")
+ticker = input()
+print(f"You want to trade {ticker}")
+print("What date do you want to trade?")
+date = input()
+print(f"You want to trade {ticker} on {date}")
+print("Running the trading agent...")
+# forward propagate
+_, decision = ta.propagate(ticker, date)
+print("The trading agent has finished running")
+print(f"The decision is {decision}")
+
+# Memorize mistakes and reflect
+# ta.reflect_and_remember(1000) # parameter is the position returns
